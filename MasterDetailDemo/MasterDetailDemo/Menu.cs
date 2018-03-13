6348ce2070b1541f470160258acdeec519f395c4
Ayman Gassara <a.gassara@root.sa>
2018-03-13 15:21:51 +0100

    Add project files.

---
+++ MasterDetailDemo/MasterDetailDemo/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterDetailDemo
+{
+    public class Menu
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
